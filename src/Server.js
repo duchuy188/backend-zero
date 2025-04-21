3b597ac1f9e7766a5f2d9b82f87bc29b39fa94ee
duchuy188 <huyndse173390@fpt.edu.vn>
2025-04-21 21:40:07 +0700

    .

--- src/Server.js
+++ src/Server.js
@@ -1,13 +1,19 @@
 const express = require('express')
 const app = express()
-const port = 3001
+const path = require('path')
+require('dotenv').config()
+
+const port = process.env.PORT || 3000
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs')
 
 app.get('/abc', (req, res) => {
-  res.send('Check abc')
+  res.render('sample.ejs')
 })
 
 app.get('/', (req, res) => {
-  res.send('Hello World! hello huy')
+  res.send('Hello World! hello huy and han')
 })
 
 app.listen(port, () => {
